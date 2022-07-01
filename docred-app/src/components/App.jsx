12af74a32ad9bde633e6df9a165f54986529c732
jeyfredc <jeyfredc@gmail.com>
2022-06-30 22:07:41 -0500

    Se realiza contenedores para aplicar imagenes de noticias y textos correspondientes

--- docred-app/src/components/App.jsx
+++ docred-app/src/components/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import ContentCenter from './LandingPage/ContentCenter'
 import NavBar from './LandingPage/NavBar'
 
 const App = () => {
   return (
+    <Fragment>
     <NavBar/>
+    
+    <ContentCenter/>
+    </Fragment>
   )
 }
 
---
+++ docred-app/src/components/LandingPage/ContentCenter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../../styles/LandingPage/ContentCenter.scss'
+
+const ContentCenter = () => {
+  return (
+    <div className="content__center">
+	<div className='article__center'> 
+    
+    <div>
+        1
+    </div>
+    
+    </div>
+	<div > 2 </div>
+</div>
+  )
+}
+
+export default ContentCenter
