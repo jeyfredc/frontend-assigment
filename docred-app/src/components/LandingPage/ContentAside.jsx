d89edc908bb533305cda7cc6fce10e499ae47839
jeyfredc <jeyfredc@gmail.com>
2022-07-01 21:13:44 -0500

    Se agrega maquetado de contenido, y barra lateral, seccion noticas relacionadas, pendiente agregar funcionalidad a los botones que dicen Noticia

---
+++ docred-app/src/components/LandingPage/ContentAside.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import fontImage from "../../assets/fontImg.png";
+import "../../styles/LandingPage/ContentAside.scss";
+
+const ContentAside = () => {
+  return (
+    <div className="conten__aside_article">
+      <div id="titulo">
+        <p>
+          <b> Noticias relacionadas</b>
+        </p>
+        <hr className="line" />
+      </div>
+      <div className="content__notice__article">
+        <img src={fontImage} alt="noticia" className="imageNoticeMini imgMini" />
+        <button>Noticia</button>
+        <h5 className="titleMini">Lorem ipsum, dolor sit amet consectetur </h5>
+        <p className="textMini">Por: Lorem ipsum dolor </p>
+        <div className="view__article">
+          <ul className="view__article__buttons">
+            <li>
+              <i className="pi pi-star"></i>50
+            </li>
+            <li>
+              <i className="pi pi-comment"></i>40
+            </li>
+            <li>
+              <i className="pi pi-eye"></i>75
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr className="separation"/>
+      <div className="content__notice__article">
+        <img src={fontImage} alt="noticia" className="imageNoticeMini imgMini" />
+        <button>Noticia</button>
+        <h5 className="titleMini">Lorem ipsum, dolor sit amet consectetur </h5>
+        <p className="textMini">Por: Lorem ipsum dolor </p>
+        <div className="view__article">
+          <ul className="view__article__buttons">
+            <li>
+              <i className="pi pi-star"></i>50
+            </li>
+            <li>
+              <i className="pi pi-comment"></i>40
+            </li>
+            <li>
+              <i className="pi pi-eye"></i>75
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr className="separation"/>
+      <div className="content__notice__article">
+        <img src={fontImage} alt="noticia" className="imageNoticeMini imgMini" />
+        <button>Noticia</button>
+        <h5 className="titleMini">Lorem ipsum, dolor sit amet consectetur </h5>
+        <p className="textMini">Por: Lorem ipsum dolor </p>
+        <div className="view__article">
+          <ul className="view__article__buttons">
+            <li>
+              <i className="pi pi-star"></i>50
+            </li>
+            <li>
+              <i className="pi pi-comment"></i>40
+            </li>
+            <li>
+              <i className="pi pi-eye"></i>75
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr className="separation"/>
+      <div className="content__notice__article">
+        <img src={fontImage} alt="noticia" className="imageNoticeMini imgMini" />
+        <button>Noticia</button>
+        <h5 className="titleMini">Lorem ipsum, dolor sit amet consectetur </h5>
+        <p className="textMini">Por: Lorem ipsum dolor </p>
+        <div className="view__article">
+          <ul className="view__article__buttons">
+            <li>
+              <i className="pi pi-star"></i>50
+            </li>
+            <li>
+              <i className="pi pi-comment"></i>40
+            </li>
+            <li>
+              <i className="pi pi-eye"></i>75
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContentAside;
--- docred-app/src/components/LandingPage/ContentCenter.jsx
+++ docred-app/src/components/LandingPage/ContentCenter.jsx
@@ -1,19 +1,111 @@
-import React from 'react'
-import '../../styles/LandingPage/ContentCenter.scss'
+import React from "react";
+import "../../styles/LandingPage/ContentCenter.scss";
+import fontImage from "../../assets/fontImg.png";
+import logoNotice from "../../assets/notice-docred.png";
+import ContentAside from "./ContentAside";
 
 const ContentCenter = () => {
   return (
-    <div className="content__center">
-	<div className='article__center'> 
-    
-    <div>
-        1
-    </div>
-    
-    </div>
-	<div > 2 </div>
-</div>
-  )
-}
 
-export default ContentCenter
+      <div className="content__center">
+        <div className="article__center">
+          <div className="article_center__description">
+            <button>Noticia</button>
+            <img src={fontImage} alt="noticia" className="imageNotice" />
+            <div className="article__button">
+              <h3>Lorem ipsum dolor sit amet</h3>
+              <button>Guardar</button>
+            </div>
+            <div className="section__notice">
+              <img src={logoNotice} alt="noticas docred" className="item-a" />
+              <h4 className="item-b">Noticias DocRed</h4>
+              <p className="item-c">
+                hace 12 h | Publicado: 17/01/18 | Lectura: 3 min
+              </p>
+            </div>
+            <div className="article__links">
+              <ul>
+                <li>Medicina interna</li>
+                <li>Pediatría</li>
+                <li>Obstreticia</li>
+              </ul>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+              porro delectus id cupiditate voluptatem autem, deserunt distinctio
+              expedita amet sunt unde non veniam excepturi est praesentium,
+              quasi nemo fugit magni! Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Ratione adipisci quod similique accusantium
+              molestias perspiciatis impedit hic aliquid dolor, neque officia,
+              cum eius doloremque eos vero consequuntur reprehenderit nulla.
+              Quasi!
+            </p>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+              porro delectus id cupiditate voluptatem autem, deserunt distinctio
+              expedita amet sunt unde non veniam excepturi est praesentium,
+              quasi nemo fugit magni! Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Ratione adipisci quod similique accusantium
+              molestias perspiciatis impedit hic aliquid dolor, neque officia,
+              cum eius doloremque eos vero consequuntur reprehenderit nulla.
+              Quasi!
+            </p>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+              porro delectus id cupiditate voluptatem autem, deserunt distinctio
+              expedita amet sunt unde non veniam excepturi est praesentium,
+              quasi nemo fugit magni! Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Ratione adipisci quod similique accusantium
+              molestias perspiciatis impedit hic aliquid dolor, neque officia,
+              cum eius doloremque eos vero consequuntur reprehenderit nulla.
+              Quasi!
+            </p>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+              porro delectus id cupiditate voluptatem autem, deserunt distinctio
+              expedita amet sunt unde non veniam excepturi est praesentium,
+              quasi nemo fugit magni! Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Ratione adipisci quod similique accusantium
+              molestias perspiciatis impedit hic aliquid dolor, neque officia,
+              cum eius doloremque eos vero consequuntur reprehenderit nulla.
+              Quasi!
+            </p>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+              porro delectus id cupiditate voluptatem autem, deserunt distinctio
+              expedita amet sunt unde non veniam excepturi est praesentium,
+              quasi nemo fugit magni! Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Ratione adipisci quod similique accusantium
+              molestias perspiciatis impedit hic aliquid dolor, neque officia,
+              cum eius doloremque eos vero consequuntur reprehenderit nulla.
+              Quasi!
+            </p>
+            <div className="final__article">
+              <ul className="final_article__send">
+                <li>
+                  <i className="pi pi-star"></i>Destacar
+                </li>
+                <li>
+                  <i className="pi pi-comment" />
+                  Comentar
+                </li>
+                <li>
+                  <i className="pi pi-arrow-up-right" />
+                  Compartir
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="article__center__aside">
+          <ContentAside/>
+      </div>
+    </div>
+  );
+};
+
+export default ContentCenter;
